aaf9be3ab474ef2a621fa46da0bf1fc146c5d0ba
Development - CharCoin <development@charcoin.org>
2025-08-18 19:34:21 +0530

    Header, Hero, FQAs, Footer created.

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
 import HeroSection from "@/components/sections/HeroSection";
-import TokenomicsSection from "../components/sections/TokenomicsSection";
-import StorySection from "@/components/sections/StorySection";
-import VestingSection from "../components/sections/VestingSection";
-import RoadmapSection from "../components/sections/RoadmapSection";
 import FAQs from "@/components/sections/FAQs";
 
 
